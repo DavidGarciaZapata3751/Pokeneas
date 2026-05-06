0e21f2a855d327e18b037038a5788d68f49f11f9
DavidGarciaZapata3751 <dgarciaz1@eafit.edu.co>
2026-05-06 17:18:24 -0500

    feat: add pokeneas data array with initial character definitions

--- src/data/pokeneas.js
+++ src/data/pokeneas.js
@@ -4,7 +4,7 @@ const pokeneas = [
     nombre: "Mamoncillo Reventao",
     altura: "0.4 m",
     habilidad: "Anchar las camisas",
-    imagen: "https://storage.googleapis.com/pokeneasbucket/Mamoncillo-Reventao.png",
+    imagen: "https://storage.googleapis.com/bucketpokeneas/Mamoncillo-Reventao.png",
     fraseFilosofica:
       "Pa’ que tanto lío, pa’ que tanto afán, suelte ese mamoncillo y chupe con calma.",
   },
@@ -13,7 +13,7 @@ const pokeneas = [
     nombre: "Chichon de Piso",
     altura: "0.6 m",
     habilidad: "Usar metro gratis",
-    imagen: "https://storage.googleapis.com/pokeneasbucket/Chichon-de-Piso.png",
+    imagen: "https://storage.googleapis.com/bucketpokeneas/Chichon-de-Piso.png",
     fraseFilosofica:
       "Chiquito pero peligroso",
   },
@@ -22,7 +22,7 @@ const pokeneas = [
     nombre: "Boquineto",
     altura: "0.5 m",
     habilidad: "Hablar en egipciano",
-    imagen: "https://storage.googleapis.com/pokeneasbucket/Boquineto.png",
+    imagen: "https://storage.googleapis.com/bucketpokeneas/Boquineto.png",
     fraseFilosofica:
       "Cierre la boca que se le meten moscas",
   },
@@ -31,7 +31,7 @@ const pokeneas = [
     nombre: "Patecumbia",
     altura: "0.7 m",
     habilidad: "Caminar con estilo",
-    imagen: "https://storage.googleapis.com/pokeneasbucket/Patecumbia.png",
+    imagen: "https://storage.googleapis.com/bucketpokeneas/Patecumbia.png",
     fraseFilosofica:
       "Si no baila cumbia, no es de los míos",
   },
@@ -40,7 +40,7 @@ const pokeneas = [
     nombre: "Pelo de Escoba",
     altura: "1.2 m",
     habilidad: "Barrer el piso",
-    imagen: "https://storage.googleapis.com/pokeneasbucket/Pelo-de-Escoba.png",
+    imagen: "https://storage.googleapis.com/bucketpokeneas/Pelo-de-Escoba.png",
     fraseFilosofica:
       "Con un poquito de pelo de escoba barre toda la basura",
   },
@@ -49,7 +49,7 @@ const pokeneas = [
     nombre: "Care Lapida",
     altura: "0.3 m",
     habilidad: "Guardar silecio",
-    imagen: "https://storage.googleapis.com/pokeneasbucket/Care-Lapida.png",
+    imagen: "https://storage.googleapis.com/bucketpokeneas/Care-Lapida.png",
     fraseFilosofica:
       "Calladito se ve más bonito",
   },
@@ -58,7 +58,7 @@ const pokeneas = [
     nombre: "Bola 8",
     altura: "0.8 m",
     habilidad: "Mimetización en la oscuridad",
-    imagen: "https://storage.googleapis.com/pokeneasbucket/Bola-8.png",
+    imagen: "https://storage.googleapis.com/bucketpokeneas/Bola-8.png",
     fraseFilosofica:
       "Las personas que más callan son las que menos hablan",
   },
@@ -67,7 +67,7 @@ const pokeneas = [
     nombre: "Tripa Seca",
     altura: "0.9 m",
     habilidad: "Vaciar la nevera",
-    imagen: "https://storage.googleapis.com/pokeneasbucket/Tripa-Seca.png",
+    imagen: "https://storage.googleapis.com/bucketpokeneas/Tripa-Seca.png",
     fraseFilosofica:
       "La comida de hoy está empujando la de ayer",
   },
@@ -76,7 +76,7 @@ const pokeneas = [
     nombre: "Pocillo",
     altura: "0.3 m",
     habilidad: "Oído super sónico",
-    imagen: "https://storage.googleapis.com/pokeneasbucket/Pocillo.png",
+    imagen: "https://storage.googleapis.com/bucketpokeneas/Pocillo.png",
     fraseFilosofica:
       "Uno con plata pa un carro y se compra ese pocillo, ese pocillillito...",
   },
